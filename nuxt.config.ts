908e44a4d7992d33699331af39a03211c5d62372
kuviidev <194729070+kuviidev@users.noreply.github.com>
2026-09-11 01:13:47 +0200

    feat: meta tags, nuxt upgrade

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,13 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  app: {
+    head: {
+      title: "kuvii's webby site",
+      ogTitle: "kuvii's webby site",
+      description: 'my personal website',
+      ogDescription: 'my personal website',
+    },
+  },
   ssr: true,
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
