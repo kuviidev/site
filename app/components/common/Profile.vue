c217b004c0cbc35dd8d57dfeab33535e79b2c06a
kuviidev <194729070+kuviidev@users.noreply.github.com>
2026-09-11 15:57:28 +0200

    fix: profile layout broken on lg

--- app/components/common/Profile.vue
+++ app/components/common/Profile.vue
@@ -1,5 +1,7 @@
 <template>
-  <div class="pfp flex p-4 gap-2 max-lg:gap-2 lg:gap-4 flex-col max-lg:flex-col-reverse justify-center max-lg:justify-center lg:justify-evenly">
+  <div
+    class="pfp flex p-4 gap-2 max-lg:gap-2 lg:gap-4 max-lg:flex-col-reverse justify-center max-lg:justify-center lg:justify-evenly"
+  >
     <div class="flex flex-col justify-center">
       <span class="inline-block text-xl text-blue-400 max-lg:text-center">
         kuvii
@@ -14,8 +16,6 @@
 <style lang="scss" scoped>
 .pfp {
   $alpha: 0.7;
-  background:
-    linear-gradient(rgba(#000, $alpha), rgba(#000, $alpha)),
-    url(/pfp/new_cropped.jpg);
+  background: linear-gradient(rgba(#000, $alpha), rgba(#000, $alpha)), url(/pfp/new_cropped.jpg);
 }
 </style>
