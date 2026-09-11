fabab048cc1a1d2fb43c6f348ab952ba4dce29dc
kuviidev <194729070+kuviidev@users.noreply.github.com>
2026-09-11 13:27:41 +0200

    feat: update pfp & profile component

--- app/components/common/Profile.vue
+++ app/components/common/Profile.vue
@@ -1,9 +1,21 @@
 <template>
-  <div class="flex gap-4 justify-evenly">
+  <div class="pfpbg flex gap-4 justify-evenly">
     <div class="flex flex-col justify-center">
-      <span class="text-purple-400 text-xl">kuvii</span>
-      <span class="text-zinc-400">probably online</span>
+      <span class="text-blue-400 text-xl">
+        kuvii
+        <span class="h-2 w-2 aspect-square inline-block bg-yellow rounded-full"></span>
+      </span>
+      <span class="text-yellow">probably online</span>
     </div>
-    <img src="/pfp/GoTYDkyXQAAa2EA.jpg" alt="<insert pfp here>" height="128" />
+    <img src="/pfp/new_cropped.jpg" alt="<insert pfp here>" height="128" />
   </div>
 </template>
+
+<style lang="scss" scoped>
+.pfpbg {
+  $alpha: 0.7;
+  background:
+    linear-gradient(rgba($color: #000000, $alpha: $alpha), rgba($color: #000000, $alpha: $alpha)),
+    url(/pfp/new_cropped.jpg);
+}
+</style>
